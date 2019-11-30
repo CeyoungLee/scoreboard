7452fae6240890c43d7d26fa84e28e607a44e553
Ceyoung Lee <cey.o.lee@gmail.com>
2019-11-30 15:18:00 +0900

    Player.js > even simpler es6 destructing props

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,28 +1,28 @@
 import {Counter} from "./Counter";
 import React from "react";
 
-export class Player extends React.PureComponent{
+export class Player extends React.Component{
   render(){
     console.log(this.props.name, 'rendered');
-
+    const {removePlayer, id, name, score, changeScore} = this.props;
     return (
       <div className="player">
     <span className="player-name">
-      <button className="remove-player" onClick={() => this.props.removePlayer(this.props.id)}> x </button>
-      {this.props.name}
+      <button className="remove-player" onClick={() => removePlayer(id)}> x </button>
+      {name}
     </span>
-        <Counter id={this.props.id} score={this.props.score} changeScore={this.props.changeScore}/>
+        <Counter id={id} score={this.props.score} changeScore={changeScore}/>
       </div>
     );
   }
 
-/*  shouldComponentUpdate(nextProps, nextState, nextContext) {
+ shouldComponentUpdate(nextProps, nextState, nextContext) {
     //console.log(nextProps);
     if (nextProps.score !== this.props.score){
       return true;
     }else{
       return false;
     }
-  }*/
+  }
 
 }
