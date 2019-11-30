9d703cfa4e92a361f1c4591464114fc74e436a29
Ceyoung Lee <cey.o.lee@gmail.com>
2019-11-30 12:19:44 +0900

    addPlayer validation, Stopwatch 생성

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -3,15 +3,25 @@ import React from 'react';
 export class AddPlayerForm extends React.Component {
   state = {
     value: ''
-  }
+  };
 
   handleValueChange(e) {
     console.log(e);
     this.setState({value: e.target.value});
   }
-  handleSubmit(e){
+
+  handleSubmit(e) {
     e.preventDefault();
+    //유효성체크
+    const playNode = document.getElementById('player');
+    console.log(playNode.validity.valid);
+    if(!playNode.validity.valid){
+      //isNameValid =true;
+      return;
+    }
     this.props.addPlayer(this.state.value);
+
+    //폼초기화
     this.setState({value: ''});
   }
 
@@ -20,9 +30,9 @@ export class AddPlayerForm extends React.Component {
 
     return (
       <div>
-        <form action="" className="form" onSubmit={this.handleSubmit.bind(this)}>
-          <input type="text" className="input" value={this.state.value} onChange={this.handleValueChange.bind(this)}
-                 placeholder="Enter a player's name"/>
+        <form action="" className="form" onSubmit={this.handleSubmit.bind(this)} noValidate>
+          <input id="player" type="text" className="input" value={this.state.value} onChange={this.handleValueChange.bind(this)}
+                 placeholder="Enter a player's name" required/>
           <input type="submit" className="input" value="Add Player"/>
         </form>
       </div>
