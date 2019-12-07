15d2e6b81ada594ae268ed8be8f3e02d30e9f4f0
Ceyoung Lee <cey.o.lee@gmail.com>
2019-12-07 16:16:51 +0900

    stopwatch hooks 이름 바꾸고, setInterval 을 use인터벌로 바꿈

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,39 +1,47 @@
-import React from 'react';
+import React,{useState, useEffect, useRef} from 'react';
 
-export class Stopwatch extends React.Component {
-  tickRef;
-  state = {
-    isRunning: false,
-    timer:0
-  }
+export function Stopwatch () {
+  let tickRef; //클래스에서는 속성, 펑션에서는 변수로 따로 선언.
+  const [isRunning, setIsRunning] = useState(false);
+  // 속성, 함수, = 초기값 한번에 속성 하나 정의
+  const [timer, setTimer] = useState(0);
 
-  handleStopwatch(){
-    this.setState(prevState => ({isRunning: !prevState.isRunning}))
-  }
-
-  render() {
-    return (
-      <div className="stopwatch">
-        <h1 className="h1">Stopwatch</h1>
-        <span className="stopwatch-time">{this.state.timer}</span>
-        <button onClick={this.handleStopwatch.bind(this)}>{this.state.isRunning ? 'Stop' : 'Start'}</button>
-        <button onClick={()=>{this.setState({timer:0})}}>Reset</button>
-      </div>
-    );
-  }
-
-  tick = () => {
-    if(this.state.isRunning){
-      this.setState(prevState => ({timer: prevState.timer +1})) ;
+  useInterval(()=>{
+    if (isRunning) {
+      setTimer(timer+1);
     }
-  }
+  },1000);
 
-  componentDidMount() {
-   // console.log('componentDidMount');
-    this.tickRef = setInterval(this.tick, 1000);
-  }
 
-  componentWillUnmount() {
-    clearInterval(this.tickRef);
-  }
+  return (
+    <div className="stopwatch">
+      <h1 className="h1">Stopwatch</h1>
+      <span className="stopwatch-time">{timer}</span>
+      <button onClick= {() => setIsRunning(!isRunning)}>{isRunning ? 'Stop' : 'Start'}</button>
+      <button onClick={() => {
+        setTimer(0)}}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
 }
